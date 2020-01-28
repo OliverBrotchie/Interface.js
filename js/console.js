eb34f607c697ca37615139147920435e6d19ac99
drdeathdefying <o.brotchie@gmail.com>
2020-01-28 01:16:42 +0000

    Update console.js

--- js/console.js
+++ js/console.js
@@ -1,21 +1,32 @@
 function consoleInterface(element, onMessage, options){
 
-	this.defaultOptions = {
-
-		listen: false, //true: Will print everything the browser console outputs
-		separators: 'full', // none: no separators, stripped: between messages, full: separators beteen everything [defualt]
+	this.defaultOptions = { //The defualt options for a console
 		
-		code: {
-			usage: false,
-			deliminator:'**'
+		messageOptions: {
+			separators: 'full',	// False: no separators, stripped: between messages, full: separators beteen everything [defualt]
+			includeTime: true, //Shows the time of the message
+			block: false, //Inline or block messages
+			includeTags: true, //Shows the author of the message
 		},
 
-		messageOptions: {
-			includeAuthors: false,
-			authors: {},
-			includeTime: true,
-			block: false
+		code: {
+			//False: no user code, 
+			//True: user must spesify the code with the deliminator phrase - ** //your code here **, 
+			//Auto: Will automatically detect javascript
+			usage: false,
+			deliminator:'**'//Deliminator phrase for code
+		},
+
+		parrot: { 
+			enabled: true, //Prints everything that the user inputs
+			tag: 'You' //The author tag for the user - will look in authors for styling
+		},
+
+		listen:{
+			enabled: true,  //Prints everything the browser console outputs
+			tag: 'Console' //The author tag for the browser console - will look in authors for styling
 		}
+		
 
 	}
 	
@@ -24,29 +35,25 @@ function consoleInterface(element, onMessage, options){
 	this.history = [];
 	this.options = completeAssign(this.defaultOptions,options);
 
-	function completeAssign(target, ...sources) {
-		sources.forEach(source => {
-		  let descriptors = Object.keys(source).reduce((descriptors, key) => {
-			descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
-			return descriptors;
-		  }, {});
-		  
-		  Object.getOwnPropertySymbols(source).forEach(sym => {
-			let descriptor = Object.getOwnPropertyDescriptor(source, sym);
-			if (descriptor.enumerable) {
-			  descriptors[sym] = descriptor;
-			}
-		  });
-		  Object.defineProperties(target, descriptors);
-		});
-		return target;
-	}
-
-	console.log(this.options);
 }
 
 function message(content,options){
-	this.content = content;
+
+	this.defaultContent = {
+		tag: null, //The author of the message - not required
+		text: null //The text of the message
+	}
+
+	this.defaultOptions = {
+		block: null, //Inline or block style
+		tagStyle: null, //Add css for the author '.your-class-name'
+		textStyle: null, //Add css for the text '.your-class-name'
+	}
+
+	this.content = completeAssign(this.defaultContent,content);
+	this.options = completeAssign(this.defaultOptions,options);
+	this.time = null; 
+
 }
 
 consoleInterface.prototype.clear = function(){
@@ -57,7 +64,7 @@ consoleInterface.prototype.clear = function(){
 
 consoleInterface.prototype.out = function(message){
 
-	var temp, output = "";
+	var output = '';
 
 	
 
@@ -69,6 +76,25 @@ consoleInterface.prototype.out = function(message){
 
 
 
+}
+
+//Full merge
+function completeAssign(target, ...sources) {
+	sources.forEach(source => {
+	  let descriptors = Object.keys(source).reduce((descriptors, key) => {
+		descriptors[key] = Object.getOwnPropertyDescriptor(source, key);
+		return descriptors;
+	  }, {});
+	  
+	  Object.getOwnPropertySymbols(source).forEach(sym => {
+		let descriptor = Object.getOwnPropertyDescriptor(source, sym);
+		if (descriptor.enumerable) {
+		  descriptors[sym] = descriptor;
+		}
+	  });
+	  Object.defineProperties(target, descriptors);
+	});
+	return target;
 }
 
 
