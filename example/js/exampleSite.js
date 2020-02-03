5d761f5e96005f06544f54cb426bdb9b8dcacc37
drdeathdefying <o.brotchie@gmail.com>
2020-02-03 08:10:56 +0000

    v1.5.5

--- example/js/exampleSite.js
+++ example/js/exampleSite.js
@@ -21,6 +21,9 @@ setTimeout(()=>{
 		c.out(new message({text:"or you can use console commands with //yourCommand",tag:"Console"}))
 		setTimeout(()=>{
 			c.out(new message({text:"To get more info, see: https://github.com/OliverBrotchie/Interface.js",tag:"Console"}))
+			setTimeout(()=>{
+				c.out(new message({text:"Tip - You can clear the console with: //clearHistory",tag:"Console"}))
+			},3000)
 		},1500);
 	},1500);
 },1500);
