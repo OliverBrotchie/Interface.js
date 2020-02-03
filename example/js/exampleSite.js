6a522cf660176fbff17ab8e371f1d37ef3bd1891
drdeathdefying <o.brotchie@gmail.com>
2020-02-03 06:00:13 +0000

    v1.5.1
    clearing

---
+++ example/js/exampleSite.js
@@ -0,0 +1,26 @@
+var c = new Interface(document.getElementById('example').firstElementChild,m => {},{
+
+	messageOptions: {
+		tags:{
+			tagStyles:{
+				You:"client",
+				Console:"host"
+			}
+
+		}
+	},
+	code: {
+		usage:'tagged'
+	}
+});
+
+c.out(new message({text:"Welcome to Interface.js!",tag:"Console"}));
+setTimeout(()=>{
+	c.out(new message({text:"you can use code with << your code >>",tag:"Console"}))
+	setTimeout(()=>{
+		c.out(new message({text:"or you can use console commands with //yourCommand",tag:"Console"}))
+		setTimeout(()=>{
+			c.out(new message({text:"To get more info, see: https://github.com/OliverBrotchie/Interface.js",tag:"Console"}))
+		},1500);
+	},1500);
+},1500);
--- interface/js/interface.js
+++ interface/js/interface.js
@@ -319,30 +319,3 @@ function addZero(i) {
 	}
 	return i;
 }
-
-var c = new Interface(document.getElementById('example').firstElementChild,m => {},{
-
-	messageOptions: {
-		tags:{
-			tagStyles:{
-				You:"client",
-				Console:"host"
-			}
-
-		}
-	},
-	code: {
-		usage:'tagged'
-	}
-});
-
-c.out(new message({text:"Welcome to Interface.js!",tag:"Console"}));
-setTimeout(()=>{
-	c.out(new message({text:"you can use code with << your code >>",tag:"Console"}))
-	setTimeout(()=>{
-		c.out(new message({text:"or you can use console commands with //yourCommand",tag:"Console"}))
-		setTimeout(()=>{
-			c.out(new message({text:"To get more info, see: https://github.com/OliverBrotchie/Interface.js",tag:"Console"}))
-		},1500);
-	},1500);
-},1500);
